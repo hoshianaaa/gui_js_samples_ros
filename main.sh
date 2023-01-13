c6eb97a346aa4d188f916315921a8eb9d880f5bf
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-01-14 04:42:36 +0900

    update

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 WS=catkin_ws
 
 echo " - ros server"
-#gnome-terminal --tab -t "rosbridge server" -- bash -c "source /opt/ros/*/setup.bash; source $HOME/$WS/devel/setup.bash; roslaunch  gui_js_samples_ros main.launch; exec bash"
+gnome-terminal --tab -t "rosbridge server" -- bash -c "source /opt/ros/*/setup.bash; source $HOME/$WS/devel/setup.bash; roslaunch  gui_js_samples_ros main.launch; exec bash"
 
 echo " - http server"
 gnome-terminal --tab -t "http server" -- bash -c "cd $SCRIPT_DIR/webpages; python3 -m http.server 8004; exec bash"
